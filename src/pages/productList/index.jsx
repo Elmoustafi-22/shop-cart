ec6a99d5f9d3b01339fef3aa52890697390f289e
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-12-19 11:05:40 +0100

    added some styling

--- src/pages/productList/index.jsx
+++ src/pages/productList/index.jsx
@@ -19,9 +19,9 @@ function ProductListPage() {
 
   return (
     <section className="py-12 bg-white sm:py-16 lg:py-20">
-      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+      <div className="px-4 mt-10 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="max-w-md mx-auto text-center ">
-          <h2 className="text-3xl sm:text-4xl text-gray-950 font-extralight font-worksans">Our Featured Products</h2>
+          <h2 className="text-3xl sm:text-4xl text-gray-950 font-light font-worksans">Our Featured Products</h2>
         </div>
         <div className='mt-10 lg:mt-16 grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-8'>
           {
